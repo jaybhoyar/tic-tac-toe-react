8c8520e6f3a7592f4998ea307759cf9eaa617544
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-10 00:57:57 +0530

    render

--- src/index.js
+++ src/index.js
@@ -3,14 +3,35 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: null
+		};
+	}
 	render() {
-		return <button className="square">{/* TODO */}</button>;
+		return (
+			<button className="square" onClick={() => this.props.onClick()}>
+				{this.props.value}
+			</button>
+		);
 	}
 }
 
 class Board extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			squares: Array(9).fill(null)
+		};
+	}
 	renderSquare(i) {
-		return <Square />;
+		return (
+			<Square
+				value={this.state.squares[i]}
+				onClick={() => this.handleClick(i)}
+			/>
+		);
 	}
 
 	render() {
